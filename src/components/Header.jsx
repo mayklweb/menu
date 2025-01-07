2626ad309804cfcc73274289149bb9b29dd48ab9
mayklweb <muhammadevvv@gmail.com>
2025-01-07 05:18:21 +0500

    menu

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="h">
+      <div className="container">
+        <div className="h-r">
+          <div className="h-logo">LOGO</div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Filter from "./Filter";
+import Header from "./Header";
+import ProductCard from "./ProductCard";
+
+export {Header, Filter, ProductCard}
