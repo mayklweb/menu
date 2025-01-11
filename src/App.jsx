f00ce2551af4207dd9b66c21eee7b35c292a8f66
mayklweb <muhammadevvv@gmail.com>
2025-01-11 15:03:00 +0500

    menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import { useState } from "react";
-import {
-  Filter,
-  Header,
-  Footer,
-  ProductsList,
-
-  Banner,
-} from "./components";
+import { Filter, Header, Footer, ProductsList, Banner } from "./components";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
+
+  if (window.Telegram.WebApp) {
+    const tg = window.Telegram.WebApp;
+    const initData = tg.initDataUnsafe; // Безопасный объект данных
+    const user = initData.user; // Информация о пользователе
+    console.log("User Info:", user);
+    if (user) {
+      document.getElementById(
+        "username"
+      ).innerText = `Hello, ${user.first_name}!`;
+    }
+  } else {
+    console.error("Telegram WebApp не доступен.");
+  }
+
   return (
     <>
       <Header />
