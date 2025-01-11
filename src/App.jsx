dfde22517c1fa291e5409c1cc8c0794b7d2e00b6
mayklweb <muhammadevvv@gmail.com>
2025-01-11 15:29:40 +0500

    menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
 import { useState } from "react";
 import { Filter, Header, Footer, ProductsList, Banner } from "./components";
+import { openTelegramLink } from "@telegram-apps/sdk";
+
+// import {e} from "telegram-webapps-types";
+
+// const webApp = close;
+
+// console.log(webApp.ifAvailable());
+// // webApp.ready();
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
 
-  if (window.Telegram.WebApp) {
-    const tg = window.Telegram.WebApp;
-    const initData = tg.initDataUnsafe; // Безопасный объект данных
-    const user = initData.user; // Информация о пользователе
-    console.log("User Info:", user);
-    if (user) {
-      document.getElementById(
-        "username"
-      ).innerText = `Hello, ${user.first_name}!`;
-    }
-  } else {
-    console.error("Telegram WebApp не доступен.");
-  }
+  // if (window.Telegram.WebApp) {
+  //   const tg = window.Telegr;
+  //   const initData = tg.initDataUnsafe; // Безопасный объект данных
+  //   const user = initData.user; // Информация о пользователе
+  //   console.log("User Info:", user);
+  //   if (user) {
+  //     document.getElementById(
+  //       "username"
+  //     ).innerText = `Hello, ${user.first_name}!`;
+  //   }
+  // } else {
+  //   console.error("Telegram WebApp не доступен.");
+  // }
 
   return (
     <>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import useCartStore from "../store";
+import { closeMiniApp } from "@telegram-apps/sdk";
 
 function Footer() {
   const { getTotalPrice } = useCartStore();
-
   const totalPrice = getTotalPrice()
+
+  const handleClose = closeMiniApp()
+
+
   
   return (
     <footer className="footer">
       <div className="container">
         <div className="footer-r">
           <p className="order-price">{totalPrice.toLocaleString()} so'm</p>
-          <button className="order-btn">Заказать</button>
+          <button onClick={handleClose} className="order-btn">Заказать</button>
         </div>
       </div>
     </footer>
