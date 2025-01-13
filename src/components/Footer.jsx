c720e8acd40968665c2896246f81967c473f175b
mayklweb <muhammadevvv@gmail.com>
2025-01-13 11:14:14 +0500

    menu

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,44 @@
-import React from "react";
 import useCartStore from "../store";
 import { closeMiniApp } from "@telegram-apps/sdk";
+import React, { useEffect } from "react";
+
+// import { close } from "@telegram-apps/sdk/dist/dts/scopes/components/qr-scanner/qr-scanner";
+// import { close } from "@telegram-apps/sdk/dist/dts/scopes/components/mini-app/methods";
 
 function Footer() {
   const { getTotalPrice } = useCartStore();
-  const totalPrice = getTotalPrice()
+  const totalPrice = getTotalPrice();
 
-  function hanldeColse( ){
-    if(closeMiniApp.isAvailable()){
-      close()
+  function hanldeColse() {
+    if (closeMiniApp.isAvailable()) {
+      closeMiniApp();
     }
   }
+  useEffect(() => {
+    // Ensure Telegram WebApp is initialized
+    if (typeof Telegram !== "undefined" && Telegram.WebApp) {
+      Telegram.WebApp.ready(); // Indicate the Web App is ready
+    } else {
+      console.error("Telegram WebApp is not available.");
+    }
+  }, []);
 
+  const handleClose = () => {
+    if (Telegram?.WebApp) {
+      Telegram.WebApp.close(); // Close the Web App
+    } else {
+      console.error("Telegram WebApp is not available.");
+    }
+  };
 
-
-  
   return (
     <footer className="footer">
       <div className="container">
         <div className="footer-r">
           <p className="order-price">{totalPrice.toLocaleString()} so'm</p>
-          <button onClick={hanldeColse} className="order-btn">Заказать</button>
+          <button onClick={handleClose} className="order-btn">
+            Заказать
+          </button>
         </div>
       </div>
     </footer>
