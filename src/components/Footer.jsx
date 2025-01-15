fa523274f9a63da39551af5f92767a4088e936b9
mayklweb <muhammadevvv@gmail.com>
2025-01-15 17:32:29 +0500

    menu

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -40,7 +40,7 @@ function Footer() {
     const orderData = [
       {
         tr_ID: user.id,
-        products: {cart, price: totalPrice}
+        products: {...cart}, price: totalPrice
       },
     ];
 
